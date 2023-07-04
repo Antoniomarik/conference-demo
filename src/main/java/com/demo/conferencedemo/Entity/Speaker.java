3ac6c0385b5ebcb18949b8a744abeb9ec12c68bd
Antoniomarik <antonio.marik1996@gmail.com>
2023-07-04 02:30:32 +0200

    Added Lombok

--- src/main/java/com/demo/conferencedemo/Entity/Speaker.java
+++ src/main/java/com/demo/conferencedemo/Entity/Speaker.java
@@ -1,7 +1,14 @@
 package com.demo.conferencedemo.Entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
 @Entity
 @Table(name = "speakers")
 public class Speaker {
@@ -15,54 +22,4 @@ public class Speaker {
     private String company;
     private String speaker_bio;
 
-    public Speaker() {
-    }
-
-    public Long getSpeaker_id() {
-        return speaker_id;
-    }
-
-    public void setSpeaker_id(Long speaker_id) {
-        this.speaker_id = speaker_id;
-    }
-
-    public String getFirst_name() {
-        return first_name;
-    }
-
-    public void setFirst_name(String first_name) {
-        this.first_name = first_name;
-    }
-
-    public String getLast_name() {
-        return last_name;
-    }
-
-    public void setLast_name(String last_name) {
-        this.last_name = last_name;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getCompany() {
-        return company;
-    }
-
-    public void setCompany(String company) {
-        this.company = company;
-    }
-
-    public String getSpeaker_bio() {
-        return speaker_bio;
-    }
-
-    public void setSpeaker_bio(String speaker_bio) {
-        this.speaker_bio = speaker_bio;
-    }
 }
